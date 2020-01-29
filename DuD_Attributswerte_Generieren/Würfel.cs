b663f9eac28b1cb27b1847871d2904bf8ab573fb
Raven-Witch <54775307+Raven-Witch@users.noreply.github.com>
2020-01-29 14:30:52 +0100

    Würfel hinzugefügt, mit Auswahl der Seiten sowie einer Auswählbaren Startzahl. Gibt eine Zufallszahl zurück

---
+++ DuD_Attributswerte_Generieren/Würfel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DuD_Attributswerte_Generieren
+{
+    public class Würfel
+    {
+        private int seitenZahl;
+        private int startZahl; //Die Zahl mit der der Würfel startet
+        private Random rand;
+
+        public Würfel(int seitenzahl)
+        {
+            this.seitenZahl = seitenzahl;
+            this.startZahl = 1;
+            rand = new Random();
+        }
+        public Würfel(int seitenzahl, int startzahl)
+        {
+            this.seitenZahl = seitenzahl;
+            this.startZahl = startzahl;
+            rand = new Random();
+        }
+        public int würfeln()
+        {
+            return rand.Next(this.startZahl, this.seitenZahl);
+        }
+    }
+}
